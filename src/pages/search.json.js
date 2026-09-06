2ba29f32dd649496744b79bec639259891553f05
jhoseph-est <j.ludena.v@uni.pe>
2026-09-06 11:17:55 -0500

    mejora en el buscador ahora no hace falta la tilde

--- src/pages/search.json.js
+++ src/pages/search.json.js
@@ -1,10 +1,19 @@
 import { getCollection } from 'astro:content';
 
+function limpiarTexto(texto) {
+  if (!texto) return "";
+  return texto
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase();
+}
+
 export async function GET() {
   const docs = await getCollection('docs');
   
   const searchList = docs.map(doc => ({
     title: doc.data.title,
+    cleanTitle: limpiarTexto(doc.data.title), // <-- NUEVO: Enviamos el título limpio
     url: `/docs/${doc.id.replace(/\\/g, '/')}`,
     tags: doc.data.tags || [],
     author: doc.data.author || '',
